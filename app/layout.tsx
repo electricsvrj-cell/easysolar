0fc53462e5b6baa91627b97b3a8c891b82bd33e9
Varun <270884233+electricsvrj-cell@users.noreply.github.com>
2026-09-12 12:37:25 +0530

    Add Google Analytics gtag for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5T8KKSTH');`}
         </Script>
+        
+        {/* Google Analytics */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-XRVJJD6THX" 
+          strategy="afterInteractive" 
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XRVJJD6THX');
+          `}
+        </Script>
       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-sans antialiased`}>
         <noscript>
